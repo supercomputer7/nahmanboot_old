eae4c060281aef3182ed683fa04e53241eccafc8
supercomputer7 <liavalb@gmail.com>
2019-08-01 15:11:51 +0300

    adding handling for bigger values with ports

--- bootloader/include/drivers/io/ioport.h
+++ bootloader/include/drivers/io/ioport.h
@@ -11,3 +11,17 @@ static inline uint8_t inb(uint16_t port)
                    : "Nd"(port) );
     return ret;
 }
+
+static inline void outl(uint16_t port, uint32_t val)
+{
+    asm volatile ( "outl %0, %1" : : "a"(val), "Nd"(port) );  
+}
+
+static inline uint32_t inl(uint16_t port)
+{
+    uint32_t ret;
+    asm volatile ( "inl %1, %0"
+                   : "=a"(ret)
+                   : "Nd"(port) );
+    return ret;
+}
