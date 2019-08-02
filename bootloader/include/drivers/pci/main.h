22e06860e6b6e86641dc226e8648c6bdd84ccf33
supercomputer7 <liavalb@gmail.com>
2019-08-02 09:35:27 +0300

    updating pci header types, to include definitions of pci-to-pci bridge header type and pci-to-cardbus bridge header type

--- bootloader/include/drivers/pci/main.h
+++ bootloader/include/drivers/pci/main.h
@@ -22,15 +22,15 @@ struct PCIBasicHeader {
     uint8_t progif;
     uint8_t subclass;
     uint8_t class_code;
-};
-
-struct PCIHeaderType0 {
-    struct PCIBasicHeader h;
 
     uint8_t cache_line_size;
     uint8_t latency_timer;
     uint8_t header_type;
     uint8_t bist;
+};
+
+struct PCIHeaderType0 {
+    struct PCIBasicHeader h;
 
     uint32_t bar0;
     uint32_t bar1;
@@ -54,6 +54,78 @@ struct PCIHeaderType0 {
     uint8_t interrupt_pin;
     uint8_t min_grant;
     uint8_t max_latency;
+
+} __attribute__((__packed__));
+
+struct PCIHeaderType1 {
+    struct PCIBasicHeader h;
+
+    uint32_t bar0;
+    uint32_t bar1;
+
+    uint8_t primary_bus_number;
+    uint8_t secondary_bus_number;
+    uint8_t subordinate_bus_number;
+    uint8_t secondary_latency_timer;
+
+    uint8_t io_base;
+    uint8_t io_limit;
+    uint16_t secondary_status;
+
+    uint16_t memory_base;
+    uint16_t memory_limit;
+
+    uint16_t prefetchable_memory_base;
+    uint16_t prefetchable_memory_limit;
+
+    uint32_t prefetchable_base_upper;
+    uint32_t prefetchable_limit_upper;
+
+    uint16_t io_base_upper;
+    uint16_t io_limit_upper;
+
+    uint32_t reserved;
+
+    uint32_t expansion_rom_base_address;
+
+    uint8_t interrupt_line;
+    uint8_t interrupt_pin;
+    uint16_t bridge_control;
+
+} __attribute__((__packed__));
+
+struct PCIHeaderType2 {
+    struct PCIBasicHeader h;
+
+    uint32_t cardbus_socket_base_address;
+
+    uint8_t capabilites_list_offset;
+    uint8_t reserved;
+    uint16_t secondary_status;
+
+    uint8_t pci_bus_number;
+    uint8_t cardbus_bus_number;
+    uint8_t subordinate_bus_number;
+    uint8_t cardbus_latency_timer;
+
+    uint32_t memory_base_addr0;
+    uint32_t memory_limit0;
+    uint32_t memory_base_addr1;
+    uint32_t memory_limit1;
+
+    uint32_t io_base_addr0;
+    uint32_t io_limit0;
+    uint32_t io_base_addr1;
+    uint32_t io_limit1;
+
+    uint8_t interrupt_line;
+    uint8_t interrupt_pin;
+    uint16_t bridge_control;
+
+    uint16_t subsystem_device_id;
+    uint16_t subsystem_vendor_id;
+
+    uint32_t pc_card_16bit_mode_base_addr;
 
 } __attribute__((__packed__));
 
