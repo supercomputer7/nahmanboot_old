a66d2bad666e65eb787be817495318353bc813fc
supercomputer7 <liavalb@gmail.com>
2019-09-26 16:55:45 +0300

    disabling network device, adding debugcon, simplify harddrive declaration

--- compile.sh
+++ compile.sh
@@ -1,11 +1,11 @@
 make all
 
-DRIVE_PARAMS="-drive file=vm.img,if=ide,id=drv0,format=raw"
+DRIVE_PARAMS="-hda vm.img"
 #NVME_PARAMS="-device nvme,drive=drv0,serial=foo"
 #AHCI_PARAMS="-device ide-drive,drive=drv0"
 #SECONDARY_AHCI_CONTROLLER="-device ahci,id=ahci"
 LIVE_ISO="-cdrom ../archlinux-2019.01.01-x86_64.iso"
-DEBUG_PARAMS="-no-shutdown"
+DEBUG_PARAMS="-no-shutdown -debugcon vc"
 BOOTSYSTEM_PARAMS="-boot d --enable-kvm -smp 2"
 MONITOR_PARAMS="-monitor stdio"
 CPU_PARAMS="-cpu Nehalem"
@@ -15,7 +15,7 @@ GRAPHICS_PARAMS="-vga std"
 USB_DEVICE="-device qemu-xhci"
 SOUND_DEVICE="-device AC97"
 NETWORK_DEVICE="-device rtl8139"
-DEVICES_LIST="$GRAPHICS_PARAMS $DRIVE_PARAMS $USB_DEVICE $SOUND_DEVICE $NETWORK_DEVICE"
+DEVICES_LIST="$GRAPHICS_PARAMS $DRIVE_PARAMS $USB_DEVICE $SOUND_DEVICE"
 
 #qemu-system-x86_64 -machine q35 -drive if=nvme,file=vm.img,index=0,media=disk,format=raw -m 512M -cpu qemu64 -monitor stdio #
 qemu-system-x86_64 $MACHINE_TYPE \
