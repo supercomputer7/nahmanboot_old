b2bea49f416f113288412bd63710cf0cf2dc3c6d
supercomputer7 <liavalb@gmail.com>
2019-09-26 17:47:58 +0300

    adding declaration for e820 memory map entry

---
+++ bootloader/include/drivers/kernels/e820map.h
@@ -0,0 +1,17 @@
+#ifndef E820MAP__H
+#define E820MAP__H
+
+#include <stdint.h>
+
+typedef struct e820map_entry {
+    uint32_t base_addr1;
+    uint32_t base_addr2;
+
+    uint32_t length1;
+    uint32_t length2;
+
+    uint32_t type;
+    uint32_t acpi3;
+} __attribute__((__packed__)) e820map_entry_t;
+
+#endif
