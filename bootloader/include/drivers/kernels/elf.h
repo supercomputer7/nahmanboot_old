939479c123eef390c5e49e753144d590d1146d99
supercomputer7 <liavalb@gmail.com>
2019-09-26 17:44:34 +0300

    adding support for parsing ELF executables

---
+++ bootloader/include/drivers/kernels/elf.h
@@ -0,0 +1,70 @@
+#ifndef ELF__H
+#define ELF__H
+
+#include <stdint.h>
+
+typedef struct Elf32MainHeader {
+    char magic[4];
+    uint8_t bit_width;
+    uint8_t endianess;
+    uint8_t header_version;
+    uint8_t os_abi;
+
+    char padding[8];
+
+    uint16_t file_type;
+    uint16_t instruction_set;
+
+    uint32_t elf_version;
+    uint32_t program_entry_position;
+    uint32_t program_header_table_position;
+    uint32_t section_header_table_position;
+
+    uint32_t flags;
+
+    uint16_t header_size;
+    uint16_t program_header_table_entry_size;
+    uint16_t program_header_table_entries_count;
+
+    uint16_t section_header_table_entry_size;
+    uint16_t section_header_table_entries_count;
+
+    uint16_t section_header_table_names_index;
+} __attribute__((__packed__)) Elf32MainHeader;
+
+typedef struct Elf32ProgramHeader {
+    uint32_t segment_type;
+    
+    uint32_t p_offset;
+
+    uint32_t p_vaddr;
+    uint32_t undefined;
+
+    uint32_t p_filesz;
+
+    uint32_t p_memsz;
+    uint32_t flags;
+
+    uint32_t required_alignment;
+} __attribute__((__packed__)) Elf32ProgramHeader;
+
+typedef struct Elf32_SectionHeader {
+    uint32_t sh_name;
+    uint32_t sh_type;
+
+    uint32_t sh_flags;
+    uint32_t sh_addr;
+
+    uint32_t sh_offset;
+    uint32_t sh_size;
+
+    uint32_t sh_link;
+    uint32_t sh_info;
+
+    uint32_t sh_addralign;
+    uint32_t sh_entsize;
+} __attribute__((__packed__)) Elf32_Shdr;
+
+void parseELF(Elf32MainHeader* main_header);
+
+#endif
