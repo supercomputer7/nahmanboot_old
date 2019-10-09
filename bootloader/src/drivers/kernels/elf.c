fca5dd1ce6b46eac4dcb80131e0468852e0b61d5
supercomputer7 <liavalb@gmail.com>
2019-10-09 16:09:23 +0300

    Adding ELF64 definition and reading & allow specifiying location of section data

--- bootloader/src/drivers/kernels/elf.c
+++ bootloader/src/drivers/kernels/elf.c
@@ -1,6 +1,6 @@
 #include "drivers/kernels/elf.h"
 
-void parseELF(Elf32MainHeader* main_header)
+void parseELF(Elf32MainHeader* main_header,uint32_t section_data)
 {
     Elf32ProgramHeader* progs = (Elf32ProgramHeader*)((uint32_t)main_header + (main_header->program_header_table_position));
     for(int i=0; i < main_header->program_header_table_entries_count ; ++i)
@@ -21,7 +21,39 @@ void parseELF(Elf32MainHeader* main_header)
     }
 
     uint8_t* buf3 = (uint8_t*)((uint32_t)main_header + main_header->section_header_table_position);
-    uint8_t* buf4 = (uint8_t*)(0x400000);
+    uint8_t* buf4 = (uint8_t*)(section_data);
+    for(int i=0; i < main_header->section_header_table_entries_count ; ++i)
+    {
+        // copy p_filesz bytes from p_offset to p_vaddr
+        for(uint32_t  j=0; j < main_header->section_header_table_entries_count*main_header->section_header_table_entry_size; ++j)
+        {
+            buf4[j] = buf3[j];
+        }
+    }
+}
+
+void parseELF64(Elf64MainHeader* main_header,uint32_t section_data)
+{
+    Elf64ProgramHeader* progs = (Elf32ProgramHeader*)((uint32_t)main_header + (main_header->program_header_table_position));
+    for(int i=0; i < main_header->program_header_table_entries_count ; ++i)
+    {
+        // zeroing p_memsz bytes at p_vaddr
+        uint8_t* buf = (uint8_t*)(progs[i].p_vaddr);
+        for(uint32_t j=0; j < progs[i].p_memsz; ++j)
+        {
+            buf[j] = 0;
+        }
+        
+        // copy p_filesz bytes from p_offset to p_vaddr
+        uint8_t* buf2 = (uint8_t*)((uint32_t)main_header + (progs[i].p_offset));
+        for(uint32_t  j=0; j < progs[i].p_filesz; ++j)
+        {
+            buf[j] = buf2[j];
+        }
+    }
+
+    uint8_t* buf3 = (uint8_t*)((uint32_t)main_header + main_header->section_header_table_position);
+    uint8_t* buf4 = (uint8_t*)(section_data);
     for(int i=0; i < main_header->section_header_table_entries_count ; ++i)
     {
         // copy p_filesz bytes from p_offset to p_vaddr
